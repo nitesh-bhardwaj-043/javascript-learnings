47c1f91c43be7442ba7fd478322f813a381f7549
nitesh-bhardwaj-043 <nitesh123bhardwaj123@gmail.com>
2025-01-28 18:39:11 +0530

    variables

---
+++ tutorials/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountName = "Nitesh"
+var accountPassword = "11223344"
+accountCity = "ABCD"
+
+console.log(accountName)
+
+console.table([accountId,accountName,accountPassword,accountCity])
